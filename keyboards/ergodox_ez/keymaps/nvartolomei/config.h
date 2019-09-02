cb93b0768df98628e779b3517b6c22fd33c54c1d
Nicolae Vartolomei <me@nvartolomei.com>
2019-09-02 22:55:22 +0100

    ergodox_ez/keymaps/nvartolomei: Lower debounce trying to make kbd respond quicker to keypresses and increase mouse speed

--- keyboards/ergodox_ez/keymaps/nvartolomei/config.h
+++ keyboards/ergodox_ez/keymaps/nvartolomei/config.h
@@ -2,5 +2,16 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#undef DEBOUNCE
+#define DEBOUNCE 10
+
 #undef IGNORE_MOD_TAP_INTERRUPT
 
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 16
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 12
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 15
